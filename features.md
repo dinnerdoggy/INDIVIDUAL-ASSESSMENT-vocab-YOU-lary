1b2d9e341cc1dffe0b3b174fd783be6b7e93534a
Casey <dinnerdoggy@gmail.com>
2024-08-31 11:31:55 -0500

    settup up a planning read me file. Some light styling on login page

---
+++ features.md
@@ -0,0 +1,4 @@
+Login page
+Page for cards
+create card page
+
--- public/main.js
+++ public/main.js
@@ -5,12 +5,8 @@ import '../styles/main.scss';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
+    <h1>Welcome to Vocab-YOU-lary</h1>
+    <button class="btn btn-danger" id="click-me">Login</button><br />
   `;
   console.warn('YOU ARE UP AND RUNNING!');
 
