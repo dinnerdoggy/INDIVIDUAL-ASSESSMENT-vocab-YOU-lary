68521ce504291f5d279c6993859691f3e5b56c3f
Casey <dinnerdoggy@gmail.com>
2024-09-03 18:44:06 -0500

    login bug

--- public/main.js
+++ public/main.js
@@ -2,14 +2,17 @@
 // import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import loginButton from '../components/loginButton';
+import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
-  <div class="container">
-    <h1>Welcome to Vocab-YOU-lary</h1>
-    <button class="btn btn-danger" id="click-me">Login</button>
-  </div>
+    <div id="click-me">
+    ${loginButton()}
+    </div>
   `;
+
+  // <button class="btn btn-danger" id="click-me">Login</button> for button if you have to
 
   document
     .querySelector('#click-me')
@@ -17,6 +20,7 @@ const init = () => {
 
   // USE WITH FIREBASE AUTH
   // ViewDirectorBasedOnUserAuthStatus();
+  ViewDirectorBasedOnUserAuthStatus();
 };
 
 init();
