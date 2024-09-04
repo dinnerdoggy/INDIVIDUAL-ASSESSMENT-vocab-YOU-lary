8dab126f68375463fd5fdeceaee6c34c1e91343e
Casey <dinnerdoggy@gmail.com>
2024-09-03 20:12:31 -0500

    login successful

--- public/main.js
+++ public/main.js
@@ -8,16 +8,11 @@ import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 const init = () => {
   document.querySelector('#app').innerHTML = `
     <div id="click-me">
-    ${loginButton()}
     </div>
   `;
 
   // <button class="btn btn-danger" id="click-me">Login</button> for button if you have to
-
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
+  loginButton();
   // USE WITH FIREBASE AUTH
   // ViewDirectorBasedOnUserAuthStatus();
   ViewDirectorBasedOnUserAuthStatus();
