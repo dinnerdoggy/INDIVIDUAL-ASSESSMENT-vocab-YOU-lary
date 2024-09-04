d55491f5c28ca3cc093dff38425fee6674820311
Casey <dinnerdoggy@gmail.com>
2024-09-03 21:39:11 -0500

    navBar debugging

--- public/main.js
+++ public/main.js
@@ -1,20 +1,8 @@
-// USE WITH FIREBASE AUTH
-// import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
-import 'bootstrap'; // import bootstrap elements and js
+import 'bootstrap';
 import '../styles/main.scss';
-import loginButton from '../components/loginButton';
 import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <div id="click-me">
-    </div>
-  `;
-
-  // <button class="btn btn-danger" id="click-me">Login</button> for button if you have to
-  loginButton();
-  // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
   ViewDirectorBasedOnUserAuthStatus();
 };
 
