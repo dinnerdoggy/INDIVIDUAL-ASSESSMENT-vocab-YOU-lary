7d693938f35c77d10b8c18c4d36f990e6b7a8ab2
Casey <dinnerdoggy@gmail.com>
2024-09-07 12:47:59 -0500

    fixed 404 bug by insuring every property in fb has a value

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -4,9 +4,7 @@ const navEvents = () => {
   document.querySelector('#navigation').addEventListener('click', (e) => {
     // Click event for showing the form
     if (e.target.id.includes('add-form-btn')) {
-      console.warn('Before techForm function call');
       techForm();
-      console.warn('After function call');
     }
   });
 };
